6b4a6c29bb38e190a5bc732ad5b538427b3c5a68
Manish Chetpalli <manishkumarchetpalli@gmail.com>
2025-08-19 21:55:24 +0530

    Update and rename createdataframe.py to introtopandas.py

--- IntrotoPandas/introtopandas.py
+++ IntrotoPandas/introtopandas.py
@@ -4,3 +4,7 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     column_names = ["student_id", "age"]
     result_dataframe = pd.DataFrame(student_data, columns=column_names)
     return result_dataframe
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    print(players)
+    return list(players.shape)
