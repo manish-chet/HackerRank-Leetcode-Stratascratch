6ffc067377ee6e0ea068487eb2ba2ec5a025afe3
Manish Chetpalli <kasojusathviksantos@cityuniversity.edu>
2025-08-20 17:13:11 +0530

    custom

--- Database/easy /Game-Play-Analysis.sql
+++
@@ -1,4 +0,0 @@
-select player_id,
-       min(event_date) as first_login 
-FROM activity
-group by player_id
---
+++ Database/easy.sql
@@ -0,0 +1,69 @@
+----------Duplicate-Emails----------
+SELECT  email
+FROM person
+GROUP BY email
+HAVING COUNT(*) > 1
+
+----------TRIANGLE-JUDGEMENT----------
+SELECT x, y, z,
+       CASE 
+         WHEN x + y > z AND x + z > y AND y + z > x 
+           THEN 'Yes'   -- It **is** a valid triangle
+         ELSE 'No'      -- It is **not** a valid triangle
+       END AS triangle
+FROM triangle;
+
+----------Employee-bonus----------
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b 
+    ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
+
+----------Employee-earning-more-than-manager----------
+
+SELECT e1.Name AS Employee 
+FROM Employee e1 
+JOIN Employee e2 ON e1.ManagerId = e2.Id 
+WHERE e1.Salary > e2.Salary;
+
+
+SELECT e.Name AS Employee
+FROM Employee e
+WHERE e.Salary > (
+  SELECT m.Salary
+  FROM Employee m 
+  WHERE m.id = e.ManagerId)
+
+----------Gameplay-Analysis----------
+select player_id,
+       min(event_date) as first_login 
+FROM activity
+group by player_id
+
+----------Delete-duplciate-emails----------
+DELETE p1 
+FROM Person p1, Person p2
+WHERE p1.email = p2.email
+AND p1.id > p2.id;
+
+----------Customer-who-never-order----------
+SELECT name AS Customers 
+FROM Customers
+WHERE id NOT IN (SELECT customerId  
+                 FROM Orders);
+                 
+
+SELECT name AS Customers
+FROM Customers c
+LEFT JOIN Orders o 
+ON c.id = o.customerId
+WHERE o.CustomerId IS NULL;
+
+
+SELECT name AS Customers 
+FROM Customers c
+WHERE NOT EXISTS (
+    SELECT customerId 
+    FROM Orders o
+    WHERE c.id = o.customerId);
